7a55cf1e540497a85a1b6b82e5b45928c8324028
Pablo <pablo.r.c@live.com>
2021-02-12 12:54:27 +0100

    component page done

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <!-- <img alt="Vue logo" src="./assets/logo.png">-->
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Latexpage msg="First page"/>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+//import HelloWorld from './components/HelloWorld.vue'
+
+import Latexpage from  './components/Latexpage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    //HelloWorld,
+    Latexpage,
   }
 }
 </script>
 
 <style>
-#app {
+/*#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+}*/
 </style>
